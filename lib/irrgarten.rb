3b9778428c6ebd03fe9960b6674098c1868f4530
manulg48 <lopezmanuel@correo.ugr.es>
2023-10-05 10:34:25 +0200

    añadidos los require_relative

--- lib/irrgarten.rb
+++ lib/irrgarten.rb
@@ -1,4 +1,9 @@
 #encoding: utf-8
+require_relative 'weapon'
+require_relative 'shield'
+require_relative 'dice'
+require_relative 'game_state'
+
 module Irrgarten
     module directions
         LEFT =:left 
