a824b6ebbed1917e597ddcb934de72610ddd59a2
manulg48 <lopezmanuel@correo.ugr.es>
2023-10-12 17:36:23 +0200

    implementacion de la clase monster

---
+++ lib/monster.rb
@@ -0,0 +1,35 @@
+class Monster
+  @@INITAL_HEALTH = 5
+  def initialise(name,intelligence,strength,health,row,col)
+    @name = name
+    @intelligence = intelligence
+    @strength = strength
+    @health = health
+    @row = row
+    @col = col
+  end
+  def dead
+    
+  end
+
+  def attack
+    
+  end
+
+  def defend(recieved_attack)
+    
+  end
+  
+  def set_pos(row,col)
+    
+  end
+  
+  def to_String
+    
+  end
+
+  def got_wounded
+
+  end
+
+end
