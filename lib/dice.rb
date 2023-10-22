4c53effff6c55124f9807496d17b013b7007211d
Isaac Morales Santana <102816007+imorlxs@users.noreply.github.com>
2023-10-22 19:37:56 +0200

    Arreglados los espacios

--- lib/dice.rb
+++ lib/dice.rb
@@ -9,19 +9,23 @@ class Dice
         @@max_attack = 3
         @@max_shield = 2
         @@generator = Random.new
-        
+
         def self.random_pos(max)
             @@generator.rand(max)
         end   
+        
         def self.who_starts(n_players)  
             @@generator.rand(n_players)
         end 
+        
         def self.random_intelligence
             @@generator.rand(@@max_intelligence)
         end
+        
         def self.random_strength
             @@generator.rand(@@max_strength)
         end
+        
         def self.resurrect_player
             if @@generator.rand(1.0) < @@resurrect_prob 
                 resurrect = true
@@ -29,32 +33,37 @@ class Dice
                 resurrect = false
             end
         end
+        
         def self.weapons_reward
             @@generator.rand(@@weapons_reward + 1)
         end
+        
         def self.shields_reward
             @@generator.rand(@@shield_reward + 1)
         end 
+        
         def self.health_reward
             @@generator.rand(@@health_reward + 1)
-        end     
+        end  
+        
         def self.weapon_power
             @@generator.rand(@@max_attack + 1)
         end
+        
         def self.shield_power
             @@generator.rand(@@max_shield + 1)
         end
+        
         def self.uses_left
             @@generator.rand(@@max_uses + 1)
         end
+        
         def self.intensity(competencia)
             @@generator.rand(competencia + 1)
         end 
+        
         def self.discard_element(uses_left)
-            if @@generator.rand(@@max_uses + 1) > uses_left 
-                true
-            else
-                false
-            end
+             @@generator.rand(@@max_uses + 1) > uses_left  
         end 
+        
     end
