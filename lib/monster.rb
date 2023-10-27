eb4f4528f141a2c3ee6ae2215c223a7e57d0b3a5
manuel <lopezmanuel@correo.ugr.es>
2023-10-27 16:01:05 +0200

    Documentacion

--- lib/monster.rb
+++ lib/monster.rb
@@ -8,26 +8,29 @@ class Monster
     @row = row
     @col = col
   end
+  #@brief Checks if the monster is dead. Return a true if health is lower or equal to zero
   def dead
     
   end
-
+  #@brief Return a value generated from the dice class method intensity passing the strength attribute as argument
   def attack
     
   end
-
+  #Proxima Practica
   def defend(recieved_attack)
     
   end
-  
+  #@brief Refresh the position of the monster
+  #@param row Fila nueva
+  #@param col Columna nueva
   def set_pos(row,col)
     
   end
-  
+  #@brief Return the a string containing the stats of the mosnter
   def to_String
     
   end
-
+  # @brief Decrease one unit the health of the monster
   def got_wounded
 
   end
