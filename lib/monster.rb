0d2b345f1e0d494a5cfc57abb9621ed55df6d2c2
Pablo <sorianopablo@correo.ugr.es>
2023-10-28 20:17:17 +0200

    Implementacion clase Monster

--- lib/monster.rb
+++ lib/monster.rb
@@ -1,20 +1,26 @@
+require_relative 'Dice'
+
 class Monster
   @@INITAL_HEALTH = 5
-  def initialise(name,intelligence,strength,health,row,col)
+  def initialise(name,intelligence,strength,row,col)
     @name = name
     @intelligence = intelligence
     @strength = strength
-    @health = health
+    @health = @@INITAL_HEALTH
     @row = row
     @col = col
   end
   #@brief Checks if the monster is dead. Return a true if health is lower or equal to zero
   def dead
     
+    @health <= 0
+
   end
   #@brief Return a value generated from the dice class method intensity passing the strength attribute as argument
   def attack
     
+    Dice.intensity(@strength)
+
   end
   #Proxima Practica
   def defend(recieved_attack)
@@ -25,13 +31,29 @@ class Monster
   #@param col Columna nueva
   def set_pos(row,col)
     
+    @row = row
+    @col = col
+
   end
   #@brief Return the a string containing the stats of the mosnter
   def to_String
     
+    estado = ""
+
+    estado << @name
+    estado << @intelligence
+    estado << @strength
+    estado << @health
+    estado << @row
+    estado << @col
+
+    estado
+
   end
   # @brief Decrease one unit the health of the monster
   def got_wounded
+
+    @health -= 1
 
   end
 
